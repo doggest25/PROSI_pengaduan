8855964835385ef9ed2d00b9cfae4ff03e566439
Agusty Labdanayoga <yogest86@gmail.com>
2024-05-05 01:49:13 -0700

    perbaikan title page

--- resources/views/layout/template.blade.php
+++ resources/views/layout/template.blade.php
@@ -7,7 +7,7 @@ scratch. This page gets rid of all links and provides the needed markup only.
 <head>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
-  <title>AdminLTE 3 | Top Navigation</title>
+  <title>Peduli Lingkungan</title>
 
   <meta name="csrf-token" content="{{ csrf_token() }}"> <!-- Untuk mengirimkan token Laravel CSRF pada setiap request ajax -->
 
--- resources/views/layouts/template.blade.php
+++ resources/views/layouts/template.blade.php
@@ -3,7 +3,7 @@
 <head>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
-  <title>{{ config('app.name', 'PWL – Laravel Starter Code') }}</title>
+  <title>Peduli Lingkungan</title>
 
   <meta name="csrf-token" content="{{ csrf_token() }}"> <!-- Untuk mengirimkan token Laravel CSRF pada setiap request ajax -->
   
