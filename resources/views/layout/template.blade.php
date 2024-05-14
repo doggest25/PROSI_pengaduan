891467038c54917337207f0a336a669c3179103a
Agusty Labdanayoga <yogest86@gmail.com>
2024-05-14 01:36:32 -0700

    template-html

--- resources/views/layout/template.blade.php
+++ resources/views/layout/template.blade.php
@@ -38,17 +38,7 @@ scratch. This page gets rid of all links and provides the needed markup only.
   <!-- Content Wrapper. Contains page content -->
   <div class="content-wrapper">
     <!-- Content Header (Page header) -->
-    <div class="content-header">
-      <div class="container">
-        <div class="row mb-2">
-          <div class="col-sm-6">
-            @yield('title')
-            
-          </div><!-- /.col -->
-         <!-- /.col -->
-        </div><!-- /.row -->
-      </div><!-- /.container-fluid -->
-    </div>
+    
     <!-- /.content-header -->
 
     <!-- Main content -->
--- resources/views/warga/dashboard/dashboard.blade.php
+++ resources/views/warga/dashboard/dashboard.blade.php
@@ -1,12 +1,54 @@
 @extends('layout.template')
 
-@section('title')
-@endsection
+
 @section('content')
-@if(session('success'))
-<div class="alert alert-success">{{ session('success') }}</div>
-@endif
 
+<section id="main">
+    <div class="container">
+        <div class="row">
+                    <div id="sidebar" class="col-4 col-12-medium">
+                        <section>
+                            <ul class="divided">
+                                <li>
 
+                                    <!-- Highlight -->
+                                        <article class="box highlight">
+                                            <header>
+                                                <h3><a href="#">Adukan aduanmu disini !</a></h3>
+                                            </header>
+                                            <a href="#" class="image left"><img src="{{asset('temp/images/pic06.jpg')}}" alt="" /></a>
+                                            <p>Phasellus sed laoreet massa id justo mattis pharetra. Fusce suscipit ligula vel quam
+                                            viverra sit amet mollis tortor congue magna lorem ipsum dolor et quisque ut odio facilisis
+                                            convallis. Etiam non nunc vel est suscipit convallis non id orci. Ut interdum tempus
+                                            facilisis convallis. Etiam non nunc vel est suscipit convallis non id orci.</p>
+                                            <ul class="actions">
+                                                <li><a href="{{url('warga/pengaduan')}}" class="button icon solid fa-file">Adukan !</a></li>
+                                            </ul>
+                                        </article>
 
+                                </li>
+                                <li>
+
+                                    <!-- Highlight -->
+                                        <article class="box highlight">
+                                            <header>
+                                                <h3><a href="#">Something of less note</a></h3>
+                                            </header>
+                                            <a href="#" class="image left"><img src="{{asset('temp/images/pic06.jpg')}}" alt="" /></a>
+                                            <p>Phasellus sed laoreet massa id justo mattis pharetra. Fusce suscipit ligula vel quam
+                                            viverra sit amet mollis tortor congue magna lorem ipsum dolor et quisque ut odio facilisis
+                                            convallis. Etiam non nunc vel est suscipit convallis non id orci. Ut interdum tempus
+                                            facilisis convallis. Etiam non nunc vel est suscipit convallis non id orci.</p>
+                                            <ul class="actions">
+                                                <li><a href="{{url('warga/detail')}}" class="button icon solid fa-file">History Pengaduan</a></li>
+                                            </ul>
+                                        </article>
+
+                                </li>
+                            </ul>
+                        </section>
+                    </div>
+        </div>
+    </div>               
+</section>
 @endsection
