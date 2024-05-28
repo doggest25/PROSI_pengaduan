fd1220880c8235ac77cf90f321f426758330f5da
Rio Bagas <hafizhrabbanim3@gmail.com>
2024-05-28 13:26:02 +0700

    dadasdsad

--- resources/views/layout/header.blade.php
+++ resources/views/layout/header.blade.php
@@ -55,9 +55,9 @@
     <ul class="order-1 order-md-3 navbar-nav navbar-no-expand ml-auto">
       @auth
     <li class="nav-item">
-        <span class="nav-link">
-            <i class="fas fa-user"></i> {{ Auth::user()->nama }}
-        </span>
+      <span class="nav-link" data-toggle="modal" data-target="#profileModal">
+        <i class="fas fa-user"></i> {{ Auth::user()->nama }}
+      </span>
     </li>
     <li class="nav-item">
         <a href="{{ route('logout') }}" class="nav-link" onclick="event.preventDefault(); confirmLogout();">
@@ -80,3 +80,51 @@
     </ul>
   </div>
 </nav>
+
+<div class="modal fade" id="profileModal" tabindex="-1" role="dialog" aria-labelledby="profileModalLabel" aria-hidden="true">
+  <div class="modal-dialog" role="document">
+    <div class="modal-content">
+      <div class="modal-header" >
+        <h5 class="modal-title" id="profileModalLabel">Profile</h5 >
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close" style="background-color: red">
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div>
+      <div class="modal-body">
+        <!-- Your profile widget starts here -->
+        <div class="card card-widget widget-user" >
+          <div class="widget-user-header bg-white" >
+            <h3 class="widget-user-username">{{ Auth::user()->nama }}</h3>
+            <h5 class="widget-user-desc">Warga</h5>
+          </div>
+          <div class="widget-user-image">
+            <img src="{{ asset('adminlte/logo/page.png') }}" alt="AdminLTE Logo" class="brand-image img-circle elevation-3" style="opacity: .8">
+          </div>
+          <div class="card-footer">
+            <div class="row">
+              <div class="col-sm-4 border-right">
+                <div class="description-block">
+                  <h5 class="description-header">Username</h5>
+                  <span class="description-text">{{ Auth::user()->username }}</span>
+                </div>
+              </div>
+              <div class="col-sm-4 border-right">
+                <div class="description-block">
+                  <h5 class="description-header">Alamat</h5>
+                  <span class="description-text">{{ Auth::user()->alamat }}</span>
+                </div>
+              </div>
+              <div class="col-sm-4">
+                <div class="description-block">
+                  <h5 class="description-header">Telepon</h5>
+                  <span class="description-text">{{ Auth::user()->telepon }}</span>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <!-- Your profile widget ends here -->
+      </div>
+    </div>
+  </div>
+</div>
